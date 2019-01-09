278d201322751e35591fedf0f9f69a1364e8fe79
ethan-dowler <Ethan.dowler@daveramsey.com>
2019-01-08 21:18:37 -0600

    annotate config.rb

--- config.rb
+++ config.rb
@@ -4,9 +4,11 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+# set the local build path to the root of the Github Pages repo
 set :build_dir, 'build/ethan-dowler.github.io'
 
 configure :development do
+  # turn off Google Analytics while in development mode.
   config[:disable_ga] = true
 end
 
@@ -16,13 +18,16 @@ end
 
 require "sprockets/es6"
 activate :sprockets do |s|
+  # transpile all .es6 files using sprockets
   s.supported_output_extensions << ".es6"
 end
 
+# enable the evil icon helpers
 require "evil_icons"
 helpers EvilIcons::Helpers
 after_configuration do
   sprockets.append_path(EvilIcons.assets_dir)
 end
 
+# all website urls to leave off "index.html"
 activate :directory_indexes
